caa9dd0600562b707d754a96c93dcd5806143c65
miku0 <miku.f.810129@gmail.com>
2023-07-08 03:26:31 +0000

    initial commit

--- run_search4.py
+++ run_search4.py
@@ -8,11 +8,11 @@ command_template = "~/Nominatim/build/nominatim search --query '{}' --format deb
 query_placeholder = '神奈川県幸区南加瀬3丁目8-33'
 #num_addresses = 2
 
-# 住所リストの読み込み
+# read addresses
 with open(address_file, 'r', encoding='utf-8') as file:
     selected_addresses = [line.strip() for line in file.readlines()]
 
-# コマンドのクエリ部分を置換して実行
+# run the command of query
 start_time = time.time()
 results = []
 for address in selected_addresses:
@@ -20,11 +20,11 @@ for address in selected_addresses:
     try:
         output = subprocess.check_output(command, shell=True, stderr=subprocess.DEVNULL).decode("utf-8").strip()
         result = int(output.split("\n")[-1])
-        #print(f'住所: {address}\t検索結果: {result}件')
+        #print(f'address: {address}\t result: {result}件')
         results.append(f'{address} {result}')
     except subprocess.CalledProcessError:
         results.append(f'{address} false')
-        #print(f'住所: {address}\t検索結果の取得に失敗しました')
+        #print(f'address: {address}\t false')
 end_time = time.time()
 elapsed_time = end_time - start_time
 #print('処理が完了しました。')
